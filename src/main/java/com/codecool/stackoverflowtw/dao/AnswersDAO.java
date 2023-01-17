8a42ac99742f78b31707599b8029c9c459ca6d99
BrownieMaar <barnamarci07@gmail.com>
2023-01-17 14:19:20 +0100

    implement answerDAO methods

--- src/main/java/com/codecool/stackoverflowtw/dao/AnswersDAO.java
+++ src/main/java/com/codecool/stackoverflowtw/dao/AnswersDAO.java
@@ -1,5 +1,10 @@
 package com.codecool.stackoverflowtw.dao;
 
+import com.codecool.stackoverflowtw.dao.model.Answer;
+
+import java.util.List;
+
 public interface AnswersDAO {
     int getAnswerCountByQuestionId(int questionId);
+    List<Answer> getAnswersByQuestionId(int questionId);
 }
--- src/main/java/com/codecool/stackoverflowtw/dao/AnswersDaoJdbc.java
+++ src/main/java/com/codecool/stackoverflowtw/dao/AnswersDaoJdbc.java
@@ -1,6 +1,14 @@
 package com.codecool.stackoverflowtw.dao;
 
+import com.codecool.stackoverflowtw.dao.model.Answer;
 import com.codecool.stackoverflowtw.dao.model.Database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AnswersDaoJdbc implements AnswersDAO {
     Database database;
@@ -11,6 +19,41 @@ public class AnswersDaoJdbc implements AnswersDAO {
 
     @Override
     public int getAnswerCountByQuestionId(int questionId) {
+        String template = "SELECT count(id) as result FROM answers WHERE question_id = ? GROUP BY question_id;";
+        try (Connection connection = database.getConnection();
+             PreparedStatement statement = connection.prepareStatement(template)) {
+            statement.setInt(1, questionId);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                return resultSet.getInt("result");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return 0;
+    }
+
+    @Override
+    public List<Answer> getAnswersByQuestionId(int questionId) {
+        String template = "SELECT * FROM answers WHERE question_id = ?";
+        List<Answer> answers = new ArrayList<>();
+        try (Connection connection = database.getConnection();
+             PreparedStatement statement = connection.prepareStatement(template)) {
+            statement.setInt(1, questionId);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                answers.add(new Answer(
+                        resultSet.getInt("id"),
+                        resultSet.getString("answer"),
+                        resultSet.getTimestamp("created").toLocalDateTime(),
+                        resultSet.getInt("question_id"),
+                        resultSet.getInt("user_id")
+
+                ));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return answers;
     }
 }
