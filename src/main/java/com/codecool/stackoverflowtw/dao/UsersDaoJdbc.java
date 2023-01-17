1b190c0ad33ed6ba9cc6ecfdec40b20943beb3e3
BrownieMaar <barnamarci07@gmail.com>
2023-01-17 12:34:55 +0100

    rename users daojdbc

--- src/main/java/com/codecool/stackoverflowtw/dao/UsersDaoJdbc.java
+++ src/main/java/com/codecool/stackoverflowtw/dao/UsersDaoJdbc.java
@@ -1,6 +1,6 @@
 package com.codecool.stackoverflowtw.dao;
 
-public class UsersDAOJdbc implements UsersDAO {
+public class UsersDaoJdbc implements UsersDAO {
     @Override
     public String getUsernameFromUserId(int id) {
         return null;
