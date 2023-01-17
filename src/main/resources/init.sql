0e98cba7b91b549356e4aa64ac634dd7c2aaabd3
BrownieMaar <barnamarci07@gmail.com>
2023-01-17 10:32:06 +0100

    write tables and example fields

---
+++ src/main/resources/init.sql
@@ -0,0 +1,44 @@
+DROP TABLE IF EXISTS questions CASCADE;
+DROP TABLE IF EXISTS answers CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+
+CREATE TABLE users
+(
+    id       SERIAL PRIMARY KEY,
+    name     VARCHAR(255) UNIQUE,
+    password VARCHAR(80)
+);
+
+CREATE TABLE questions
+(
+    id       SERIAL PRIMARY KEY,
+    question VARCHAR(255),
+    user_id  INT,
+    FOREIGN KEY (user_id)
+        REFERENCES users (id)
+);
+
+CREATE TABLE answers
+(
+    id          SERIAL PRIMARY KEY,
+    answer      VARCHAR(255),
+    question_id INT,
+    user_id     INT,
+    FOREIGN KEY (question_id)
+        REFERENCES questions (id),
+    FOREIGN KEY (user_id)
+        REFERENCES users (id)
+);
+
+INSERT INTO users (name, password) VALUES ('Siyar', 'tevedurumteve');
+INSERT INTO users (name, password) VALUES ('Zoli', 'taborialpha');
+INSERT INTO users (name, password) VALUES ('Dénes', 'questionman');
+INSERT INTO users (name, password) VALUES ('Marci', '1_10_11_100');
+
+
+INSERT INTO questions (question, user_id) VALUES ('Are camels polyamouros?', 1);
+INSERT INTO questions (question, user_id) VALUES ('Wath is the best OOP? With Regards, D.', 3);
+
+INSERT INTO answers (answer, question_id, user_id) VALUES ('No.', 1, 2);
+INSERT INTO answers (answer, question_id, user_id) VALUES ('I have made out with a camel already, but I might have dreamed that.', 1, 4);
+INSERT INTO answers (answer, question_id, user_id) VALUES ('Check out this juicy video about it!', 2, 3);
