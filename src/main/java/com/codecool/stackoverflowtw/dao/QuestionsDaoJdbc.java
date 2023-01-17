fb31b065bc36d04421b5f459a05355f7678b8d28
BrownieMaar <barnamarci07@gmail.com>
2023-01-17 11:13:34 +0100

    implement getAllQuestions from the interface

--- src/main/java/com/codecool/stackoverflowtw/dao/QuestionsDaoJdbc.java
+++ src/main/java/com/codecool/stackoverflowtw/dao/QuestionsDaoJdbc.java
@@ -1,8 +1,40 @@
 package com.codecool.stackoverflowtw.dao;
 
+import com.codecool.stackoverflowtw.controller.dto.QuestionDTO;
+import com.codecool.stackoverflowtw.dao.model.Database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuestionsDaoJdbc implements QuestionsDAO {
+    Database database;
+
+    public QuestionsDaoJdbc(Database database) {
+        this.database = database;
+    }
+
     @Override
     public void sayHi() {
         System.out.println("Hi DAO!");
+    }
+
+    @Override
+    public List<QuestionDTO> getAllQuestions() {
+        String template = "SELECT * FROM questions";
+        List<QuestionDTO> questions = new ArrayList<>();
+        try (Connection connection = database.getConnection();
+             PreparedStatement statement = connection.prepareStatement(template)) {
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                questions.add(new QuestionDTO(resultSet.getInt(1), resultSet.getString(2), resultSet.getString(3), resultSet.getTimestamp(4).toLocalDateTime(), resultSet.getInt(5) ));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return questions;
     }
 }
