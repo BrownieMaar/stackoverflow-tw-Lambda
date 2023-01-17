4fbefe5e1f901ca5dec13187bd8fdf8e669d1c2b
BrownieMaar <barnamarci07@gmail.com>
2023-01-17 11:12:29 +0100

    add a getAllQuestion to the QuestionsDAO interface

--- src/main/java/com/codecool/stackoverflowtw/dao/QuestionsDAO.java
+++ src/main/java/com/codecool/stackoverflowtw/dao/QuestionsDAO.java
@@ -1,5 +1,11 @@
 package com.codecool.stackoverflowtw.dao;
 
+import com.codecool.stackoverflowtw.controller.dto.QuestionDTO;
+
+import java.util.List;
+
 public interface QuestionsDAO {
     void sayHi();
+
+    List<QuestionDTO> getAllQuestions();
 }
